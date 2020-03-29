4fddbfa7972125bc6397d02962c103e34d92129c
CallMeHK <hkrazy888@gmail.com>
2020-03-29 14:16:14 -0600

    messing with websockets idk

--- socket.js
+++ socket.js
@@ -1,11 +1,26 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 
+io.origins('*:*')
+
 io.on('connection', function(client) {
+  client.on('connect', () => {
+    console.log('connected!')
+    return 'connected!!!!!'
+  })
+
+  let i = 0
+
+  const emitInterval = setInterval(() => {
+    client.emit('message', i + ': This is an interval lmao')
+    i++
+  }, 3000)
+
   client.on('message', () => 'this is a message')
 
   client.on('disconnect', function() {
     console.log('client disconnect...', client.id)
+    clearInterval(emitInterval)
   })
 
   client.on('error', function(err) {
@@ -14,7 +29,7 @@ io.on('connection', function(client) {
   })
 })
 
-server.listen(3000, function(err) {
+server.listen(3001, function(err) {
   if (err) throw err
-  console.log('listening on port 3000')
+  console.log('listening on port 3001')
 })
